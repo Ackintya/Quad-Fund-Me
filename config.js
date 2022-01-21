fb8ff316bca5a580d420b4c333d2604d70c54ee7
Achin <64888458+d-bugg@users.noreply.github.com>
2022-01-21 22:24:18 +0530

    address change

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const marketaddress="0x99ED04a2D01C16a3c94D0f9fB7F624B07C9DefCE"
+export const marketaddress="0x8803a07EE25a5C4005358c1f986D3c07947CaD26"
 //export const projectaddress="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 //export const pooladdress="0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
